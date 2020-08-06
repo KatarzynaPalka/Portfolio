5e963b82d6e449e3061ff6161eb60fea7fbd3aeb
KatarzynaPalka <katarzynapalka928@gmail.com>
2020-08-06 22:12:10 +0200

    Dodano funkcjonalność tagów

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Portfolio
 Przykładowe projekty i aplikacje
 1. BLOG
-1	Kim był Django Reinhardt	kim-byl-django-reinhardt	Django Reinhardt, właśc. Jean-Baptiste Reinhardt – francuski gitarzysta i kompozytor romskiego pochodzenia urodzony w Belgii. Reinhardt uważany jest za jednego z najwybitniejszych gitarzystów wszech czasów.	opublikowany
-2	Nowy tytuł posta	jeszcze-jeden-post	To jest tresc posta.	
-3	Inny post	inny-post	To jest tresc innego posta.	
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 
 # Create your views here.
@@ -18,8 +19,13 @@ class PostListView(ListView):
 
 
 # II sposób na widok
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -31,7 +37,8 @@ def post_list(request):
     return render(request,
                   'blog/post/list.html',
                   {'page': page,
-                   'posts': posts})
+                   'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -39,12 +46,25 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
 
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'comment_form': comment_form})
 
-# rozwijamy formularze
 
 def post_share(request, post_id):
+    # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
 
@@ -52,17 +72,18 @@ def post_share(request, post_id):
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            post_url = request.build_absolute_url(
+            post_url = request.build_absolute_uri(
                 post.get_absolute_url())
             subject = '{} ({}) zachęca do przeczytania "{}"'.format(cd['name'], cd['email'], post.title)
-            message = 'Przeczytaj post "{}" na stronie {}\n\n Komentarz dodany przez {}: {}'.format(post.title,
-                                                                                                    post_url,
-                                                                                                    cd['name'],
-                                                                                                    cd['comments'])
-            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            message = 'Przeczytaj post "{}" na stronie {}\n\n{}\' Komentarz dodany przez: {}'.format(post.title,
+                                                                                                     post_url,
+                                                                                                     cd['name'],
+                                                                                                     cd['comments'])
+            send_mail(subject, message, 'katarzynapalka928@gmail.com',
+                      [cd['to']])
             sent = True
-        else:
-            form = EmailPostForm()
-        return render(request, 'blog/post/share.html', {'post': post,
-                                                        'form': form,
-                                                        'sent': sent})
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
