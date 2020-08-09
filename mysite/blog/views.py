0bc2e10d7cbf2ddec2e80bad2bd16f0fee58aa58
KatarzynaPalka <katarzynapalka928@gmail.com>
2020-08-09 10:22:44 +0200

    Kanał wiadomości RSS 2

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post, Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
+
 
 
 # Create your views here.
@@ -59,10 +61,10 @@ def post_detail(request, year, month, day, post):
         comment_form = CommentForm()
 
     post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
         .exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
-        .order_by('-same_tags', '-publish')[:4]
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+                        .order_by('-same_tags', '-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
@@ -95,3 +97,41 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
                                                     'sent': sent})
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.objects.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.3).order_by('-similarity')
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
+
+# def post_search(request):
+#     form = SearchForm()
+#     if 'query' in request.GET:
+#         form = SearchForm(request.GET)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             results = SearchQuerySet().models(Post)\
+#                                       .filter(content=cd['query']).load_all()
+#             # Obliczenie całkowitej liczby wyników.
+#             total_results = results.count()
+#             return render(request,
+#                           'blog/post/search.html',
+#                           {'form': form,
+#                           'cd': cd,
+#                           'results': results,
+#                           'total_results': total_results})
+#     else:
+#         return render(request,
+#                       'blog/post/search.html',
+#                       {'form': form})
